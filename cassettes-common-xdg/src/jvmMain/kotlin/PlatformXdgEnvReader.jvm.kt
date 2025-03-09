ea7dc79f9ebb1e2f35f71561e37d36da3d97bfbe
Alexey Illarionov <alexey@0xdc.ru>
2025-03-09 13:50:03 +0300

    fix typo

--- cassettes-common-xdg/src/jvmMain/kotlin/PlatformXdgEnvReader.jvm.kt
+++ cassettes-common-xdg/src/jvmMain/kotlin/PlatformXdgEnvReader.jvm.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2024-2025 Alexey Illarionov and the cassetes-kmp project contributors
+ * SPDX-FileCopyrightText: 2024-2025 Alexey Illarionov and the cassettes-kmp project contributors
  * SPDX-License-Identifier: Apache-2.0
  */
 
